47d426e0b28e66538a7458b11ba8649640367c6c
Minkhant <mkt293822@gmail.com>
2024-11-16 11:12:07 +0630

    video can be send

---
+++ Messenger/static/js/script.js
@@ -0,0 +1,139 @@
+ // JavaScript to toggle sidebar visibility
+      function toggleSidebar() {
+        var sidebar = document.getElementById("sidebar");
+        sidebar.classList.toggle("active");// Toggle 'active' class to show/hide sidebar
+        console.log('Hello world');
+}
+      
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownButton = document.querySelector(".dropdown-button");
+
+if (dropdownButton) {
+  dropdownButton.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
+    console.log('Hello')
+  });
+}
+
+window.onload = function () {
+        const messagesContainer = document.querySelector(".container");
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
+};
+      
+
+
+
+// like message action
+const likesvg = `
+   <svg width="3pc" height="3pc" fill="#2050ca" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+ 
+    <g id="Page-1"  fill-rule="evenodd">
+        <g id="Dribbble-Light-Preview" transform="translate(-259.000000, -760.000000)" fill="#2050ca">
+            <g id="icons" transform="translate(56.000000, 160.000000)">
+                <path d="M203,620 L207.200006,620 L207.200006,608 L203,608 L203,620 Z M223.924431,611.355 L222.100579,617.89 C221.799228,619.131 220.638976,620 219.302324,620 L209.300009,620 L209.300009,608.021 L211.104962,601.825 C211.274012,600.775 212.223214,600 213.339366,600 C214.587817,600 215.600019,600.964 215.600019,602.153 L215.600019,608 L221.126177,608 C222.97313,608 224.340232,609.641 223.924431,611.355 L223.924431,611.355 Z" id="like-[#1385]">
+
+</path>
+            </g>
+        </g>
+    </g>
+</svg>
+` 
+const likeBtn = document.querySelector('.like__container');
+const roomId = document.getElementById('roomId').value;
+const url1 = `/like_send/${roomId}/`
+
+likeBtn.addEventListener('click', () => {
+  //console.log('haha')
+  fetch(url1, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-CSRFToken': getCookie('csrftoken')
+    },
+    body: new URLSearchParams({
+      svg: likesvg
+    })
+  }).then(response => response.text()).then(data => {
+    console.log(`success${data}`);
+  }).catch(error => {
+    console.error(error);
+  });
+  location.reload();
+});
+
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++){
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return decodeURIComponent(cookie.substring(name.length + 1));
+    }
+  }
+  return null
+}
+
+let messageButtons = document.querySelectorAll('.message__button');
+
+messageButtons.forEach(element => {
+  if (element.querySelector('svg')) {
+    element.style.background = 'none';
+    element.style.border = 'none';
+  } 
+});
+
+
+// Image send read file
+
+const imageBtn = document.querySelector('.gallery__container');
+const imageInput = document.getElementById('image_input');
+
+imageBtn.addEventListener('click', () => {
+  imageInput.click()
+});
+
+imageInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const formData = new FormData();
+
+  if (file) {
+    const fileName = file.name.toLowerCase();  // Ensure case-insensitive check
+    const fileType = file.type;
+
+    // Debug: Log the file name to verify it's correct
+    console.log('Selected file:', fileName);
+    console.log('Select file type;', fileType)
+    // Check for image extensions
+    if (fileName.endsWith('.jpg') || fileName.endsWith('.png')) {
+      formData.append('img', file);     
+      console.log('File is an image:', fileName);  // Debug: log for image type
+    }
+    // Check for video (.mp4) extension
+    else if (fileName.endsWith('.mp4')||fileType==='video/mp4') {
+      formData.append('video', file);
+      console.log('File is a video:', fileName);  // Debug: log for video type
+    }
+    // If the file is neither an image nor an mp4 video, log an error
+    else {
+      console.error('Error: Unsupported file type. Only .jpg, .png, .mp4 are allowed.');
+      return;  // Exit early if the file type is unsupported
+    }
+
+   // If valid file is selected, send the data to the server
+    const url2 = `/image_send/${roomId}/`;
+    fetch(url2, {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken')
+      },
+      body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+      console.log(`success: ${data}`);
+      location.reload();  // Consider updating the UI instead of reloading
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+});
---
+++ Messenger/views.py
@@ -0,0 +1,210 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Rooms, Messages, Private_rooms, Private_messages, User_Info
+from django.contrib import messages
+from .forms import MyUserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.core.files.storage import default_storage
+import uuid
+
+
+@login_required(login_url='login')
+def home(request):
+    q = request.GET.get('q', '')  # Default to empty string if 'q' is not provided
+    user_info = User_Info.objects.get(user=request.user)
+    have_request = False
+    if user_info.request_friend.count() != 0:
+        have_request = True
+
+    # Filter friends based on search query
+    friends = user_info.friends.filter(
+        Q(first_name__icontains=q) |
+        Q(last_name__icontains=q) |
+        Q(username__icontains=q)
+    )
+
+    for friend in friends:
+        user_id = user_info.user.id
+        friend_id = friend.id
+
+        # Normalize the user order
+        user1_id = min(user_id, friend_id)
+        user2_id = max(user_id, friend_id)
+
+        # Check if a room already exists for this pair of users
+        if not Private_rooms.objects.filter(user_id=user1_id, friend_id=user2_id).exists():
+            # If not, create a new room
+            Private_rooms.objects.get_or_create(user_id=user1_id, friend_id=user2_id)
+
+    # Filter private rooms based on search query
+    private_rooms = Private_rooms.objects.filter(
+        Q(user=user_info.user) |
+        Q(friend=user_info.user)
+    ).filter(
+        Q(friend__username__icontains=q) |
+        Q(friend__last_name__icontains=q) |
+        Q(friend__first_name__icontains=q)
+    )
+
+    context = {
+        'user_info': user_info,
+        'friends': friends,
+        'private_rooms': private_rooms,
+        'have_request': have_request
+    }
+    return render(request, 'home.html', context)
+
+
+
+def message_room_view(request, pk):
+    room = Private_rooms.objects.get(id=pk)
+    messages = Private_messages.objects.filter(room=room)
+    user_info = User_Info.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        room_id = request.POST.get('room_id')
+        # print(room_id)
+        if message:
+            message = message.strip()
+            if message:
+                Private_messages.objects.create(content=message, room=room, user=request.user)
+        elif room_id:
+            room_to_delete = Private_rooms.objects.get(id=room_id)
+            if room_to_delete.user == request.user:
+                if room_to_delete.friend in user_info.friends.all():
+                    user_info.friends.remove(room_to_delete.friend)
+                    room_to_delete.friend.user_info.friends.remove(request.user)
+                    room_to_delete.delete()
+                    return redirect('home')
+            else:
+                if room_to_delete.user in user_info.friends.all():
+                    user_info.friends.remove(room_to_delete.user)
+                    room_to_delete.user.user_info.friends.remove(request.user)
+                    room_to_delete.delete()
+                    return redirect('home')
+            
+    context = {
+        'room': room,
+        'messages': messages, 
+    }
+    return render(request, 'message.html', context)
+
+
+def like_send(request, pk):
+    svg_data = request.POST.get('svg')
+    room_id = uuid.UUID(pk)
+    room = get_object_or_404(Private_rooms, id=room_id)
+    if svg_data:
+        Private_messages.objects.create(room=room, content=svg_data, user=request.user)
+        
+    return JsonResponse({'message':'success'})
+
+def image_send(request,pk):
+    img_data = None
+    video_data = None
+
+    # Check for the files in the request
+    if 'img' in request.FILES:
+        img_data = request.FILES['img']
+    if 'video' in request.FILES:
+        video_data = request.FILES['video']
+    room_id = uuid.UUID(pk)
+    room = get_object_or_404(Private_rooms, id=room_id)
+    if img_data:
+        img =  default_storage.save('images/'+img_data.name,img_data)
+        Private_messages.objects.create(room=room, image=img, user=request.user)
+    elif video_data:
+        video = default_storage.save('file/video/'+video_data.name, video_data)
+        Private_messages.objects.create(room=room, video=video, user=request.user)
+    else:
+        print('error')
+        
+    return JsonResponse({'message': 'success'})
+
+
+@login_required(login_url='login')
+def add_friend(request):
+    available = User.objects.exclude(username=request.user.username)
+    user_info = request.user.user_info
+    current_friends = user_info.friends.all()
+    pending_requests = user_info.request_friend.all()
+    
+    available_friend = available.exclude(id__in=current_friends).exclude(id__in=pending_requests)
+    if  request.method == 'POST':
+        user_id = request.POST.get('user-id')
+        user = User.objects.get(id=user_id)
+        user_info = User_Info.objects.get(user=user)
+        user_info.request_friend.add(request.user)
+    
+    context = {
+        'available_friends': available_friend,
+    }
+    return render(request, 'find_friend.html', context)
+
+
+@login_required(login_url='login')
+def requested_friend_view(request):
+    user_info = User_Info.objects.get(user=request.user)
+    requested_friends = user_info.request_friend.all()
+    if request.method == 'POST':
+        accept = request.POST.get('accept')
+        deny = request.POST.get('deny')
+        if accept:
+            accept_user= User.objects.get(id=accept)
+            user_info.friends.add(accept_user)
+            user_info.request_friend.remove(accept_user)
+            accept_user.user_info.friends.add(request.user)
+            # Private_rooms.objects.get_or_create(user=user_info.user, )
+        elif deny:
+            deny_user = User.objects.get(id=deny)
+            user_info.request_friend.remove(deny_user)
+            
+         
+    context = {
+        'requested_friends': requested_friends,
+    }
+    return render(request, 'requested_friend.html', context)
+
+#Registration
+
+def signup_view(request):
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            user_info, created = User_Info.objects.get_or_create(user=user, id=user.id)
+            user_info.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Form is invalid')
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'signup.html', context)
+
+
+def login_view(request):
+    if request.method == 'POST':
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+       user = authenticate(request, username=username, password=password)
+       if user is not None:
+           user_info, created = User_Info.objects.get_or_create(user=user)
+           user_info.save()
+           login(request, user)
+           return redirect('home')
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
